1d036637e2f11f97e71465e57e0ae88687a7a362
fiker-2025 <fikerteamenu9@gmail.com>
2025-10-29 13:59:49 +0300

    S

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -3,9 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.DateField()
-    isbn = models.CharField(max_length=13)
-    pages = models.IntegerField()
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
